dac5031bfaaf617d9c8261db92ca1b64c0f3ae6b
NewSulc <vsulc007@outlook.cz>
2023-04-08 21:03:07 +0200

    Pls be righjt

--- ZaverecnaPrace/Board.xaml.cs
+++ ZaverecnaPrace/Board.xaml.cs
@@ -14,37 +14,118 @@ using System.Windows.Shapes;
 
 namespace ZaverecnaPrace
 {
-    /// <summary>
-    /// Interaction logic for Board.xaml
-    /// </summary>
     public partial class Board : Window
     {
+        BoardClasses boardClass = new BoardClasses();
+
         public Board()
         {
             InitializeComponent();
+            boardClass.cardNumber = 0;
         }
 
-        int lastCardX = 0;
+        public string newName, newDescription;
 
         private void AddCard(object sender, RoutedEventArgs e)
         {
-            CardList.Height = CardList.ActualHeight + 50;
+            int listIndex = int.Parse((sender as FrameworkElement).Name.Substring((sender as FrameworkElement).Name.Length - 1));
+            boardClass.cardLists[listIndex].Height += 50;
+            boardClass.panelLists[listIndex].Height += 50;
+            boardClass.buttonLists[listIndex].Margin = new Thickness(0, boardClass.buttonLists[listIndex].Margin.Top + 50, 0, 0);
 
-            Thickness marginTop = AddCardButton.Margin;
-            marginTop.Top = marginTop.Top + 50;
-            AddCardButton.Margin = marginTop;
+            CardInfo info = new CardInfo();
+            info.ShowDialog();
+            AddCardToList(listIndex);
+        }
 
+        private void AddCardToList(int index)
+        {
             Grid card = new Grid();
-            card.Height = 20;
-            card.Width = 100;
 
-            Thickness cardMarginTop = card.Margin;
-            cardMarginTop.Top = lastCardX;
-            lastCardX += 50;
-            card.Margin = cardMarginTop;
+            card.MouseUp += cardClick;
 
+            card.Height = 50;
+            card.Width = 250;
+            boardClass.cardNumber++;
+            Border newBorder = new Border();
+            newBorder.BorderThickness = new Thickness(2);
+            newBorder.BorderBrush = Brushes.Black;
+            card.Children.Add(newBorder);
+     
+            TextBlock txt = new TextBlock();
+            txt.Text = newName;
+            txt.HorizontalAlignment = HorizontalAlignment.Center;
+            txt.VerticalAlignment = VerticalAlignment.Center;
+            txt.FontSize = 18;
+
+            CardClass newCard = new CardClass();
+            newCard.cardName = newName;
+            newCard.indexInList = boardClass.panelLists.Count;
+            card.Name = newCard.cardName;
+            boardClass.cardClassLists.Add(newCard);
+
+            card.Children.Add(txt);
             card.Background = Brushes.White;
-            CardList.Children.Add(card);
+            boardClass.panelLists[index].Children.Add(card);
+        }
+
+        private void cardClick(object sender, RoutedEventArgs e)
+        {
+            Grid cdString = (Grid)sender;
+
+            CardInfoChange changeCardInfo = new CardInfoChange();
+            for (int i = 0; i < boardClass.cardClassLists.Count; i++)
+            {
+                if (boardClass.cardClassLists[i].cardName == cdString.Name)
+                {
+                    changeCardInfo.name = boardClass.cardClassLists[i].cardName;
+                }
+            }
+            changeCardInfo.ShowDialog();
+        }
+
+        private void AddList(object sender, RoutedEventArgs e)
+        {
+            AddListButton.Margin = new Thickness(AddListButton.Margin.Left + 500, AddListButton.Margin.Top, AddListButton.Margin.Right, AddListButton.Margin.Bottom);
+            AddListToBoard();
+        }
+
+        private void AddListToBoard()
+        {
+            int index = boardClass.cardLists.Count;
+
+            Grid list = new Grid();
+            list.Name = "CardPanel" + index;
+            list.Height = 150;
+            list.Width = 400;
+            list.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF7C7C7C");
+            list.HorizontalAlignment = HorizontalAlignment.Left;
+            list.VerticalAlignment = VerticalAlignment.Top;
+            list.Margin = new Thickness((index)*500, 50, 0, 0);
+
+            StackPanel stackPan = new StackPanel();
+            stackPan.Name = "CardList" + index;
+            stackPan.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FFB12B2B");
+            stackPan.Margin = new Thickness(68, 10, 68, 90);
+            stackPan.Height = 0;
+
+            Button addCardBtn = new Button();
+            addCardBtn.Name = "AddCardButton" + index;
+            addCardBtn.Content = "+";
+            addCardBtn.HorizontalAlignment = HorizontalAlignment.Center;
+            addCardBtn.VerticalAlignment = VerticalAlignment.Top;
+            addCardBtn.Height = 50;
+            addCardBtn.Width = 150;
+            addCardBtn.Margin = new Thickness(0, 90, 0, 0);
+            addCardBtn.Click += AddCard;
+
+            list.Children.Add(addCardBtn);
+            list.Children.Add(stackPan);
+            MainGrid.Children.Add(list);
+
+            boardClass.cardLists.Add(list);
+            boardClass.panelLists.Add(stackPan);
+            boardClass.buttonLists.Add(addCardBtn);
         }
     }
 }
---
+++ ZaverecnaPrace/Classes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace ZaverecnaPrace
+{
+    internal class BoardClasses
+    {
+        public List<Grid> cardLists = new List<Grid>();
+        public List<StackPanel> panelLists = new List<StackPanel>();
+        public List<Button> buttonLists = new List<Button>();
+
+        public int cardNumber;
+
+        public List<CardClass> cardClassLists = new List<CardClass>();
+    }
+
+    internal class CardClass
+    {
+        public string cardName;
+        public string cardDescription;
+        public int indexInList;
+    }
+
+    internal class BoardClass
+    {
+        public List<CardClass> cardClassesList = new List<CardClass>();
+        public string boardName;
+    }
+
+    internal class ProjectClass
+    {
+        public string projectName;
+        public List<BoardClass> boardClassList = new List<BoardClass>();
+    }
+
+    internal class UserClass
+    {
+        public string userName;
+        public string userEmail;
+        public string userPassword;
+        public List<ProjectClass> projectClassList = new List<ProjectClass>();
+    }
+}
